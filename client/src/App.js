c1635b726bfec598d17f0dd920ad40ab6bbf0e69
Alex Malko <alexmalko29@gmail.com>
2020-06-07 08:53:21 -0700

    fethuser action creator added

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,33 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Routes from './components/routing/Routes';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
+import { connect } from 'react-redux';
+import { fetchUser } from './actions/TEST_auth';
 
 // Redux
-import { Provider } from 'react-redux';
 import store from './store';
 
-export default function App() {
+function App() {
 	useEffect(() => {
 		setAuthToken(localStorage.token);
 		store.dispatch(loadUser());
 	}, []);
 
+	useEffect(() => {
+		fetchUser();
+	});
+
 	return (
-		<Provider store={store}>
-			<BrowserRouter>
-				<Fragment>
-					<Navbar />
-					<Switch>
-						<Route exact path="/" component={Landing} />
-						<Route component={Routes} />
-					</Switch>
-				</Fragment>
-			</BrowserRouter>
-		</Provider>
+		<BrowserRouter>
+			<Fragment>
+				<Navbar />
+				<Switch>
+					<Route exact path="/" component={Landing} />
+					<Route component={Routes} />
+				</Switch>
+			</Fragment>
+		</BrowserRouter>
 	);
 }
+
+export default connect(null, { fetchUser })(App);
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
 	app.use(
-		[ '/auth/google', '/auth/facebook' ],
+		[ '/auth/google', '/auth/facebook', '/api/current_user' ],
 		createProxyMiddleware({
 			target: 'http://localhost:5000'
 		})
